058437d0fa7e0f94485385686f2f27a03c20743d
Lily <lilyhuang841020@gmail.com>
2022-08-02 15:18:27 +0800

    find練習

---
+++ find.js
@@ -0,0 +1,18 @@
+let ary = ["1", "10", "12", "6", "23"];
+
+let result = ary.find((items) => items > 10);
+console.log(result);
+
+//用for-loop
+function find(ary) {
+  let result = [];
+  for (let i = 0; i < ary.length; i++) {
+    if (ary[i] > 10) {
+      result.push(ary[i]);
+      break
+    }
+  }
+  return result;
+}
+
+console.log(find(ary));
